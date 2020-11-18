d37bd8c8144f87cc288f0e04d5076f742fb65987
Prasanna <prasanna.ve@ecgroup-intl.com>
2020-11-18 11:35:05 +0530

    One more commit

--- PracticeSeleniumbasics/src/HandlingAlerts.java
+++ PracticeSeleniumbasics/src/HandlingAlerts.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HandlingAlerts {
 
+	//Trying to commit
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Shalini\\eclipse-workspace\\PracticeSeleniumbasics\\drivers\\chromedriver.exe");
 			WebDriver	driver =	new ChromeDriver();
